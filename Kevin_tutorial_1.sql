ccfbe0d31a18d0873a515415b1ac2cd493c7594b
Pouya Alipour <pouya.alipour741@gmail.com>
2024-09-29 23:07:05 +0330

    updated on 29/6/2024

--- Kevin_tutorial_1.sql
+++ Kevin_tutorial_1.sql
@@ -3,3 +3,22 @@ select CustomerName as [customer] from kcc.dbo.Customers;
 select sum(OrderTotal)
 from Orders
 where OrderDate >= DATEADD(year,-3,getdate())
+
+select *
+from Orders
+where OrderDate >= DATEADD(month,-31,getdate());
+
+select sum(OrderTotal)
+from Orders
+where OrderDate >= DATEADD(month,-31,getdate());
+
+select sum(OrderTotal)
+from Orders
+where OrderDate >= DATEADD(month,-31,getdate())
+group by CustomerID;
+
+--using query designer
+SELECT        Customers.CustomerID, Orders.OrderDate, Orders.CustomerID AS Expr1, Orders.OrderTotal
+FROM            Customers INNER JOIN
+                         Orders ON Customers.CustomerID = Orders.CustomerID
+
