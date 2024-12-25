e5497d0ae70466fd91fa4339b768aeb60a4261f3
pouya alipour <pouya.alipour741@gmail.com>
2024-12-25 17:28:54 +0330

    5 dey

---
+++ work examples/وضعیت های در حال انجام.sql
@@ -0,0 +1,47 @@
+﻿--use  SAODB
+
+--فرآیند پذیرش دانشجویان قهرمان ورزشی
+select * from dbo.Tbl_CU_LogStatus
+where wid = 109
+and exactmessage not like N'%خاتمه%'
+
+----فرآیند استعلام مدرک تحصیلی 
+select * from dbo.Tbl_CU_LogStatus
+where wid = 2000556
+and exactmessage not like N'%خاتمه%'
+
+
+---فرآیند ویرایش اطلاعات سامانه مهر اداره دانش آموختگان  
+select * from dbo.Tbl_CU_LogStatus
+where wid = 2000560
+and exactmessage not like N'%خاتمه%'
+
+----فرآیند استعلام سوابق تحصیلی   
+select * from dbo.Tbl_CU_LogStatus
+where wid = 2000558  --  به اضافه کد 382
+and exactmessage not like N'%خاتمه%'
+
+
+-------فرآیند درخواست موسسات فعال اعزام دانشجو به خارج از کشور
+select * from dbo.Tbl_CU_LogStatus
+where wid = 2000561
+and exactmessage not like N'%خاتمه%'
+
+
+----فرآیند صدور مجوز موسسات جذب دانشجوی بین الملل
+select * from dbo.Tbl_CU_LogStatus
+where wid = 2000567
+and exactmessage not like N'%خاتمه%'
+
+
+----فرآیند صدور معافیت تحصیلی
+select * from dbo.Tbl_CU_LogStatus
+where wid = 2000040
+and exactmessage not like N'%خاتمه%'
+
+----صدور گواهی اشتغال به تحصیل
+select * from dbo.Tbl_CU_LogStatus
+where wid = 57
+and exactmessage not like N'%خاتمه%'
+
+
