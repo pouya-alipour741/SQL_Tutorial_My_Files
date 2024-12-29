1ff9fd378486299c7d90f633b10a9f8db0a22209
Pouya Alipour <pouya.alipour741@gmail.com>
2024-12-29 23:51:22 +0330

    update
    second method uses temp table

--- work examples/saman_query_updated_new.sql
+++ work examples/saman_query_updated_new.sql
@@ -1,3 +1,14 @@
+select top 1000 *
+into #temp
+from
+	Workflow.TblWorkflow f
+	join task.TblWorkflowInstance i on f.WorkflowId = i.WorkflowID
+	join task.TblWorkflowActivityInstance a on a.WokflowInstanceID = i.WorkflowInstanceID
+	join task.TblTask t on t.WorkflowActivityInstaceID = a.WorkflowActivityInstanceID
+	join task.TblTaskStatus s on s.TaskStatusID = t.TaskStatusID
+
+
+
 with cte as(
 select
 	f.name
@@ -5,20 +16,16 @@ select
 	,i.CreateDate
 	,case when ROW_NUMBER() over(partition by i.WorkflowInstanceID order by (select null)) = 1 then i.WorkflowInstanceID end as distinct_WorkflowInstanceID
 from
-	Workflow.TblWorkflow f
-	join task.TblWorkflowInstance i on f.WorkflowId = i.WorkflowID
-	join task.TblWorkflowActivityInstance a on a.WokflowInstanceID = i.WorkflowInstanceID
-	join task.TblTask t on t.WorkflowActivityInstaceID = a.WorkflowActivityInstanceID
-	join task.TblTaskStatus s on s.TaskStatusID = t.TaskStatusID
+	#temp
 )
 ,cte2 as(
 select
-	Name,
-	last_VALUE(CreateDate) over(partition by Name order by CreateDate rows between unbounded preceding and unbounded following) last_CreateDate
+	Name
+	,LAST_VALUE(CreateDate) over(partition by Name order by CreateDate rows between unbounded preceding and unbounded following) last_CreateDate
 	,count(distinct_WorkflowInstanceID) over(partition by Name) wf_count
 	,count(case when TaskStatusID = 6 then distinct_WorkflowInstanceID end) over(partition by Name) wf_count_open
-	,count(case when TaskStatusID = 1 then distinct_WorkflowInstanceID end) over(partition by Name) wf_count_in_progress,
-	ROW_NUMBER() over(partition by name order by (select null)) rowNumber2
+	,count(case when TaskStatusID = 1 then distinct_WorkflowInstanceID end) over(partition by Name) wf_count_in_progress
+	,ROW_NUMBER() over(partition by name order by (select null)) rowNumber2
 from
 	cte
 )
