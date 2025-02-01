a96d3c2701a6d024a3c795ebd61ac18667e1cfaa
Pouya Alipour <pouya.alipour741@gmail.com>
2025-02-01 23:29:58 +0330

    13 bahman

---
+++ work examples/پشتیبانی سجاد/UpdateCursor.sql
@@ -0,0 +1,34 @@
+begin tran
+	declare @wfid int
+
+	declare UpdateCursor cursor for
+	select
+		WorkflowInstanceID
+	from
+		task.TblWorkflowInstance i
+	where
+		WorkflowId = 2000045   
+		and WorkflowInstanceStatusID != 2
+		and CreateDate < ''
+
+
+
+	open UpdateCursor
+
+	fetch next from UpdateCursor into @wfid
+
+	while @@FETCH_STATUS = 0
+		begin
+	
+			exec SP_TBS_CancelWorkflowInstance  @wfid
+
+			update Tbl_CU_QuestionAnswer
+			set StatusID = 1921
+			where WFID = @wfid
+
+			fetch next from UpdateCursor into @wfid
+		end
+
+
+	close UpdateCursor
+	deallocate UpdateCursor
