10d53756cdbecf8296d575ac4b478192e3da2439
Pouya Alipour <pouya.alipour741@gmail.com>
2025-03-02 00:43:09 +0330

    home

--- work examples/فرآیند میز خدمت فناوری اطلاعات فاز دوم/miz khedmat v2.sql
+++ work examples/فرآیند میز خدمت فناوری اطلاعات فاز دوم/miz khedmat v2.sql
@@ -9,19 +9,18 @@ exec Sp_Cu_Tbl_Cu_ServingTableSecondPhase_Log_Frm31264 @FromDate=N'',@ToDate=N'1
 
 select
 	TaskName,
-	--(select dbo.MiladiToShamsi(t.CreateDate)) CreateDate,
-	--(select dbo.MiladiToShamsi(t.EndDate)) EndDate, TaskStatusName,
 	t.CreateDate,
 	t.EndDate,
 	case
 		when isnull(t.EndDate, '') != '' then datediff(hour,t.CreateDate , t.EndDate) 
 		else datediff(hour,t.CreateDate , getdate()) 
 	end hours_diff,	
+	sum(case
+		when isnull(t.EndDate, '') != '' then datediff(hour,t.CreateDate , t.EndDate) 
+		else datediff(hour,t.CreateDate , getdate()) 
+	end) over() total_actor_time, 
 	p.FullName,
 	p.UserId
---,ActivityID
---,p.UserID
---, tr.Groupid
 from
 	task.TblWorkflowInstance i
 	join task.TblWorkflowActivityInstance a on a.WokflowInstanceID = i.WorkflowInstanceID
@@ -32,15 +31,44 @@ from
 where
 	--WorkflowID = 62
 	--and WorkflowInstanceStatusID = 2
-	 i.WorkflowInstanceID = 1852996
-	 and p.UserId <> 2
-	 and p.UserID in( SELECT u.UserId,
-           FullName
-    WHERE u.UserId IN (SELECT DISTINCT
-                              X.RegUserID
-                          FROM Tbl_Cu_ServingTableSecondPhaseHistory_Log X
-                          WHERE X.RoleID = 4
+	 i.WorkflowInstanceID = 1852996   -- = x.wfid
+	 --and p.UserId <> 2
+	 and p.UserID in( SELECT DISTINCT
+                              RegUserID
+                          FROM Tbl_Cu_ServingTableSecondPhaseHistory_Log 
+                          WHERE RoleID = 4
 						  )
-						
-	 
+
+
+
+
+
+------------------------------					
+with cte as(
+
+	,(select
+		sum(case
+			when isnull(t.EndDate, '') != '' then datediff(hour,t.CreateDate , t.EndDate) 
+			else datediff(hour,t.CreateDate , getdate()) 
+		end) --over() 
+	from
+		task.TblWorkflowInstance i
+		join task.TblWorkflowActivityInstance a on a.WokflowInstanceID = i.WorkflowInstanceID
+		join task.TblTask t on t.WorkflowActivityInstaceID = a.WorkflowActivityInstanceID
+		join users.TblProfiles p on p.UserId = t.UserId	
+	where
+		 i.WorkflowInstanceID =    --  x.wfid
+		 and p.UserID in( SELECT DISTINCT
+								  RegUserID
+							  FROM Tbl_Cu_ServingTableSecondPhaseHistory_Log 
+							  WHERE RoleID = 4) ) Actor_hours,										 
+	count(*) over() tickets_count
+
+
+)
+	select
+		*,
+		(sum(Actor_hours) over() / tickets_count) TicketsInActorCartable_AverageTime
+	from cte
+
 
