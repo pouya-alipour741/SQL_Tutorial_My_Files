2a1a2ca8aff5c5be3cf6feb2ac60c3f731ea22df
pouya alipour <pouya.alipour741@gmail.com>
2025-06-23 13:57:31 +0330

    2 tir

--- work examples/find items in procedures.sql
+++ work examples/find items in procedures.sql
@@ -3,5 +3,13 @@ select * from sys.procedures
 where OBJECT_DEFINITION(object_id) like N'%institudeID%'
 
 
+--get specific keywords in stored procedures
+SELECT *
+        FROM            INFORMATION_SCHEMA.ROUTINES
+WHERE        (ROUTINE_DEFINITION LIKE N'%update Tbl_Cu_RequestLanguageExam_LOG%')
+
+
 --get datatypes
 EXEC sp_describe_first_result_set N'YourStoredProcedureNameHere'
+
+
